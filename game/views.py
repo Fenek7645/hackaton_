4c62febab9da0670971d1dc0be364710bd8f65b7
Fenek7645 <artem.por2018@gmail.com>
2023-12-28 00:45:46 +0300

    qweqwewqeqew

--- game/views.py
+++ game/views.py
@@ -34,33 +34,141 @@ def game2(request):
     return render(request, 'test_game/game2.html')
 
 @login_required(login_url='')
+def game2_1(request):
+    return render(request, 'test_game/game2_1.html')
+
+@login_required(login_url='')
+def game2_2(request):
+    return render(request, 'test_game/game2_2.html')
+
+@login_required(login_url='')
+def game2_3(request):
+    return render(request, 'test_game/game2_3.html')
+
+@login_required(login_url='')
 def game3(request):
     return render(request, 'test_game/game3.html')
+
+@login_required(login_url='')
+def game3_1(request):
+    return render(request, 'test_game/game3_1.html')
+
+@login_required(login_url='')
+def game3_2(request):
+    return render(request, 'test_game/game3_3.html')
+
+@login_required(login_url='')
+def game3_3(request):
+    return render(request, 'test_game/game3_3.html')
 
 @login_required(login_url='')
 def game4(request):
     return render(request, 'test_game/game4.html')
 
 @login_required(login_url='')
+def game4_1(request):
+    return render(request, 'test_game/game4_1.html')
+
+@login_required(login_url='')
+def game4_2(request):
+    return render(request, 'test_game/game4_2.html')
+
+@login_required(login_url='')
+def game4_3(request):
+    return render(request, 'test_game/game4_3.html')
+
+@login_required(login_url='')
 def game5(request):
     return render(request, 'test_game/game5.html')
+
+@login_required(login_url='')
+def game5_1(request):
+    return render(request, 'test_game/game5_1.html')
+
+@login_required(login_url='')
+def game5_2(request):
+    return render(request, 'test_game/game5_2.html')
+
+@login_required(login_url='')
+def game5_3(request):
+    return render(request, 'test_game/game5_3.html')
 
 @login_required(login_url='')
 def game6(request):
     return render(request, 'test_game/game6.html')
 
 @login_required(login_url='')
+def game6_1(request):
+    return render(request, 'test_game/game6_1.html')
+
+@login_required(login_url='')
+def game6_2(request):
+    return render(request, 'test_game/game6_2.html')
+
+@login_required(login_url='')
+def game6_3(request):
+    return render(request, 'test_game/game6_3.html')
+
+@login_required(login_url='')
 def game7(request):
     return render(request, 'test_game/game7.html')
+
+@login_required(login_url='')
+def game7_1(request):
+    return render(request, 'test_game/game7_1.html')
+
+@login_required(login_url='')
+def game7_2(request):
+    return render(request, 'test_game/game7_2.html')
+
+@login_required(login_url='')
+def game7_3(request):
+    return render(request, 'test_game/game7_3.html')
 
 @login_required(login_url='')
 def game8(request):
     return render(request, 'test_game/game8.html')
 
 @login_required(login_url='')
+def game8_1(request):
+    return render(request, 'test_game/game8_1.html')
+
+@login_required(login_url='')
+def game8_2(request):
+    return render(request, 'test_game/game8_2.html')
+
+@login_required(login_url='')
+def game8_3(request):
+    return render(request, 'test_game/game8_3.html')
+
+@login_required(login_url='')
 def game9(request):
     return render(request, 'test_game/game9.html')
 
 @login_required(login_url='')
+def game9_1(request):
+    return render(request, 'test_game/game9_1.html')
+
+@login_required(login_url='')
+def game9_2(request):
+    return render(request, 'test_game/game9_2.html')
+
+@login_required(login_url='')
+def game9_3(request):
+    return render(request, 'test_game/game9_3.html')
+
+@login_required(login_url='')
 def game10(request):
     return render(request, 'test_game/game10.html')
+
+@login_required(login_url='')
+def game10_1(request):
+    return render(request, 'test_game/game10_1.html')
+
+@login_required(login_url='')
+def game10_2(request):
+    return render(request, 'test_game/game10_2.html')
+
+@login_required(login_url='')
+def game10_3(request):
+    return render(request, 'test_game/game10_3.html')
