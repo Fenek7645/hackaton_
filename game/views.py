288993a0b206b97c791301eced904d0c2921c1d9
Fenek7645 <artem.por2018@gmail.com>
2023-12-28 21:10:51 +0300

    йцу

--- game/views.py
+++ game/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
-
-
 
 @login_required(login_url='')
 def game(request):
